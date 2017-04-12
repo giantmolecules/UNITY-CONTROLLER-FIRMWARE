21972d681d3ecd42e2fee0a1c93ca0901f951abb
Brett Ian Balogh <brettbalogh@gmail.com>
2017-04-11 19:01:10 -0500

    commit

---
+++ UNITY-CONTROLLER-FIRMWARE/src/UNITY-CONTROLLER-FIRMWARE.ino
@@ -0,0 +1,22 @@
+/*
+ * Project UNITY-CONTROLLER-FIRMWARE
+ * Description: UDP controller for Unity
+ * Author: Brett Ian Balogh
+ * Email: brettbalogh@gmail.com
+ * Date: April 11, 2017
+ * License: CC-BY-SA-NC
+ */
+ #include "Particle.h"
+ #include "neopixel.h"
+ 
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
